45898fb86ab95feed33ad79adbd974dd25e27ae9
Ramiz <abadulla@live.ru>
2015-10-16 21:27:46 +0300

    Initial Commit

---
+++ HomeTask1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeTask1
+//
+//  Created by Ramiz on 10/16/15.
+//  Copyright (c) 2015 Ramiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HomeTask1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeTask1
+//
+//  Created by Ramiz on 10/16/15.
+//  Copyright (c) 2015 Ramiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
