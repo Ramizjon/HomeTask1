bf4a586979cdee77570e0acba742e5455d2d8e4b
Ramiz Abadulla <abadulla@live.ru>
2015-11-16 14:45:08 +0200

    New commit from bash

--- HomeTask1/AppDelegate.m
+++ HomeTask1/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+	//I just add something here
     return YES;
 }
 
@@ -26,6 +27,7 @@
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
+// and here
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
